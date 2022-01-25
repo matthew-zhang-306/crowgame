7281c3d347b6b818bfeb3f4d9fcc3cdd538e95c0
Matthew Zhang <matthew.zhang.306@gmail.com>
2022-01-25 15:58:35 -0600

    i put ramps in it but the slope movement is not very good

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,19 +17,32 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+
     private void FixedUpdate() {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo, 0.8f)) {
+            Debug.DrawRay(transform.position, hitInfo.normal, Color.red, Time.fixedDeltaTime);
+        } 
+    
+        HandleMovement();
+    }
+
+
+
+    private void HandleMovement() {
         Vector3 velocity = rb.velocity;
         
         Vector2 horizontalVel = velocity.XZ();
         Vector2 targetHorizontalVel = new Vector2(
             Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")
-        );
+        ).normalized;
         // TODO: rotate targetHorizontalVel depending on camera direction
         horizontalVel = Vector2.MoveTowards(horizontalVel, targetHorizontalVel * maxSpeed, acceleration * Time.deltaTime);
         velocity.x = horizontalVel.x;
         velocity.z = horizontalVel.y;
         
         velocity += Vector3.down * gravity * Time.deltaTime;
+        if (velocity.y < -maxFallSpeed)
+            velocity.y = maxFallSpeed;
 
         rb.velocity = velocity;
     }
