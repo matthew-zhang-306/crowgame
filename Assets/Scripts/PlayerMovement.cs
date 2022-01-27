63f8196e238a21cef433c599bd76b9b048ce592a
Matthew Zhang <matthew.zhang.306@gmail.com>
2022-01-26 23:47:17 -0600

    physics. tons of weird cases where it gets stuck on corners or misidentifies the ground its on and other weird stuff. wtf

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,40 +10,63 @@ public class PlayerMovement : MonoBehaviour
     public float maxFallSpeed;
     public float gravity;
 
-    Rigidbody rb;
+    new Rigidbody rigidbody;
+    new Collider collider;
+    Vector3 groundNormal;
 
     private void Awake() {
-        rb = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
 
 
-
     private void FixedUpdate() {
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo, 0.8f)) {
-            Debug.DrawRay(transform.position, hitInfo.normal, Color.red, Time.fixedDeltaTime);
-        } 
-    
+        groundNormal = Vector3.zero;
+        if (Physics.BoxCast(collider.bounds.center + Vector3.up * 0.1f, collider.bounds.extents, Vector3.down, out RaycastHit hit, Quaternion.identity, 0.15f)) {
+            Debug.DrawRay(transform.position, hit.normal * 2f, Color.red, Time.fixedDeltaTime);
+            groundNormal = hit.normal;
+        }
+        
         HandleMovement();
     }
 
 
 
     private void HandleMovement() {
-        Vector3 velocity = rb.velocity;
+        Vector3 velocity = rigidbody.velocity;
         
-        Vector2 horizontalVel = velocity.XZ();
-        Vector2 targetHorizontalVel = new Vector2(
-            Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")
+        Vector3 horizontalVel = velocity.WithY(0);
+        Vector3 targetHorizontalVel = new Vector3(
+            Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")
         ).normalized;
         // TODO: rotate targetHorizontalVel depending on camera direction
-        horizontalVel = Vector2.MoveTowards(horizontalVel, targetHorizontalVel * maxSpeed, acceleration * Time.deltaTime);
+        horizontalVel = Vector3.MoveTowards(horizontalVel, targetHorizontalVel * maxSpeed, acceleration * Time.deltaTime);
         velocity.x = horizontalVel.x;
-        velocity.z = horizontalVel.y;
-        
-        velocity += Vector3.down * gravity * Time.deltaTime;
-        if (velocity.y < -maxFallSpeed)
-            velocity.y = maxFallSpeed;
+        velocity.z = horizontalVel.z;
+        if (groundNormal != Vector3.zero) {
+            Vector3 groundNormalHorizontal = groundNormal.WithY(0);
+            float horizontalFactor = -Vector3.Dot(groundNormalHorizontal, horizontalVel.normalized);
+            Vector3 groundNormalUp = groundNormal - groundNormalHorizontal;
+            if (groundNormalUp.magnitude != 0)
+                velocity.y = horizontalVel.magnitude * horizontalFactor / groundNormalUp.magnitude;
+            else
+                velocity.y = 0;
 
-        rb.velocity = velocity;
+            // similar triangle relationship
+            // groundNormalUp / groundNormalProjected = horizontalVelocity / what we want
+            // what we want * groundNormalUp = horizontalVel * groundNormalProjected
+            Debug.DrawRay(transform.position, horizontalVel, Color.magenta, Time.fixedDeltaTime);
+            Debug.DrawRay(transform.position + horizontalVel, new Vector3(0, velocity.y, 0), Color.magenta, Time.fixedDeltaTime);
+        }
+        
+        if (groundNormal == Vector3.zero) {
+            velocity += Vector3.down * gravity * Time.deltaTime;
+            if (velocity.y < -maxFallSpeed)
+                velocity.y = maxFallSpeed;
+        }
+
+        Debug.DrawRay(transform.position, velocity, Color.cyan, Time.fixedDeltaTime);
+        rigidbody.velocity = velocity;
     }
+
 }
