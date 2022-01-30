189bbe011c1647aa5e270a27a50e3db2457d8132
Matthew Zhang <matthew.zhang.306@gmail.com>
2022-01-30 17:49:40 -0600

    player rises in tornado

--- Assets/Tornado.cs
+++ Assets/Tornado.cs
@@ -8,6 +8,7 @@ public class Tornado : GridAlignedObject
     public Vector3 Top => transform.position + Vector3.up * topOffset;
 
     PushableBox previousBox;
+    PlayerMovement previousPlayer;
 
     public delegate void TornadoDelegate(Tornado tornado);
     public static TornadoDelegate OnTornadoSpawned;
@@ -22,6 +23,11 @@ public class Tornado : GridAlignedObject
         OnTornadoSpawned += TornadoSpawned;
     }
     private void OnDisable() {
+        previousBox?.ExitTornado();
+        previousBox = null;
+        previousPlayer?.ExitTornado();
+        previousPlayer = null;
+
         OnTornadoSpawned -= TornadoSpawned;
     }
 
@@ -39,18 +45,35 @@ public class Tornado : GridAlignedObject
             transform.position - new Vector3(0, 0.05f, 0),
             Vector3.up,
             out RaycastHit hit,
-            topOffset,
+            topOffset + 0.1f,
             LayerMask.GetMask("Box")
         );
-        if (cast) {
-            PushableBox box = hit.rigidbody.GetComponent<PushableBox>();
-            if (box != previousBox) {
-                previousBox?.ExitTornado();
-                box?.EnterTornado(this);
-            }
-            
-            previousBox = box;
+        PushableBox box = null;
+        if (cast)
+            box = hit.rigidbody?.GetComponent<PushableBox>();
+
+        if (box != previousBox) {
+            previousBox?.ExitTornado();
+            box?.EnterTornado(this);
         }
+        previousBox = box;
+
+        cast = Physics.Raycast(
+            transform.position - new Vector3(0, 0.05f, 0),
+            Vector3.up,
+            out hit,
+            cast ? hit.distance : topOffset,
+            LayerMask.GetMask("Player")
+        );
+        PlayerMovement player = null;
+        if (cast)
+            player = hit.rigidbody?.GetComponent<PlayerMovement>();
+        
+        if (player != previousPlayer) {
+            previousPlayer?.ExitTornado();
+            player?.EnterTornado(this);
+        }
+        previousPlayer = player;
     }
 
 
