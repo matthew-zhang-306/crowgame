36192c9735949598344bfe2c1a71fc34445ebd16
Matthew Zhang <matthew.zhang.306@gmail.com>
2022-02-06 15:33:49 -0600

    create warp canvas, essentials prefab, etc

--- Assets/Scripts/WarpAltar.cs
+++ Assets/Scripts/WarpAltar.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class WarpAltar : MonoBehaviour
 {
     public string targetScene;
+    public string destinationName;
     private PlayerMovement playerInside;
+
+    public delegate void AltarDelegate(WarpAltar altar);
+    public static AltarDelegate OnAltarEnter;
+    public static AltarDelegate OnAltarExit;
+    public static AltarDelegate OnAltarWarp;
 
 
     private void Update() {
         if (playerInside != null && Input.GetAxisRaw("Action1") > 0) {
+            OnAltarWarp?.Invoke(this);
             Managers.ScenesManager.ChangeScene(targetScene, 2f);
         }
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
+            OnAltarEnter?.Invoke(this);
             playerInside = other.GetComponent<PlayerMovement>();
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.CompareTag("Player")) {
+            OnAltarExit?.Invoke(this);
             playerInside = null;
         }
     }
