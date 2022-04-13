3346f1832a65842221ee36e96916a7638bbfa1f3
joriann-marie <60859583+joriann-marie@users.noreply.github.com>
2022-04-12 22:28:26 -0500

    Added Timed Door

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -8,9 +8,12 @@ public class DoorManager : BaseSwitchable
     public Vector3 openOffset;
     private Vector3 closedPosition;
     private Vector3 openPosition;
+    private bool waiting = false;
 
     public new Collider collider;
     public Transform visualTransform;
+    public int timer = 0; //0 is default, if 0 not timed
+
 
     private void Awake() {
         closedPosition = transform.position;
@@ -24,16 +27,35 @@ public class DoorManager : BaseSwitchable
     }
 
 
-    public override void Switch() {
-        base.Switch();
+    IEnumerator timedDoor()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(timer);
+        waiting = false;
 
-        collider.transform.position = State ? openPosition : closedPosition;
-        visualTransform.DOKill();
-        visualTransform.DOMove(State ? openPosition : closedPosition, 1.0f)
-            .SetEase(Ease.InOutCubic);
-
-        Managers.AudioManager.PlaySound("door_opening");
+        //door goes back down
+        Switch(); //hopefully doesnt cause a horrible loop
     }
+
+    public override void Switch() {
+        if(!waiting)
+        {
+            base.Switch();
+
+            collider.transform.position = State ? openPosition : closedPosition;
+            visualTransform.DOKill();
+            visualTransform.DOMove(State ? openPosition : closedPosition, 1.0f)
+                .SetEase(Ease.InOutCubic);
+
+            Managers.AudioManager.PlaySound("door_opening");
+
+            if (timer > 0 && collider.transform.position == openPosition)
+            {
+                StartCoroutine(timedDoor());
+            }
+        }
+    }
+        
 
     private void OnDrawGizmos() {
         if (collider == null)
